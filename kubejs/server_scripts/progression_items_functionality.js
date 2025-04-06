6faebab253923f90594aadfe041302fc6f34c1c7
SeducedEnvelope <seducedenvelope@gmail.com>
2025-04-06 20:40:48 +0100

    solar stone and whale wind done, may need to add lingering/splash for whale wind (if i want)

--- kubejs/server_scripts/progression_items_functionality.js
+++ kubejs/server_scripts/progression_items_functionality.js
@@ -20,13 +20,88 @@ BlockEvents.rightClicked(event => {
     level.playSound(null, event.player.x, event.player.y, event.player.z, 'minecraft:entity.wither.spawn', 'master', 1, 0.5)
     item.shrink(1)
 })
+ServerEvents.loaded(event => {
+    Utils.server.scheduleInTicks(1, () => {
+        let dummy = level.createEntity('kubejs:your_projectile')
+        dummy.setPos(0, -200, 0)
+        dummy.spawn()
+        dummy.kill()
+    })
+})
+
+ServerEvents.tick(event => {
+    // Get the world where the event is running
+    // const world = event.server.level;
+    // console.log(event.server)
+    // console.log(world)
+    // const entities = event.server.level.getEntities();
+    const entities = event.server.entities
+    // if (!world) { return }
+    // if (world.entities === null) { return }
+    if (!entities) return
+    // Loop through all entities currently in the world
+    entities.forEach(entity => {
+
+        // // Example: Apply a condition for a specific entity type (e.g., 'kubejs:solar_stone_projectile')
+        // if (entity.type === 'kubejs:solar_stone_projectile') {
+            
+        //     // You can modify the entity here or check specific properties
+        //     console.log(`Found solar_stone_projectile at (${entity.x}, ${entity.y}, ${entity.z})`);
+
+        //     // // DONT WORK
+        //     // const data = entity.persistentData
+        //     // if (data.has('motion')) {
+        //     //     const motion = data.get('motion')
+        //     //     entity.setDeltaMovement(motion)
+        //     // }
+        // }
+    });
+});
+
+ItemEvents.rightClicked("kubejs:solar_stone", event => {
+    const { player, item } = event;
+    const vel = 1.5
+    // angle player looking at
+    const playerAngle = {
+        x: player.lookAngle.x(),
+        y: player.lookAngle.y(),
+        z: player.lookAngle.z()
+    }
+    // create entity to shoot
+    const entity = player.level.createEntity('kubejs:solar_stone_projectile')
+    // spawn entity
+    entity.spawn()
+    Utils.server.schedule(5, () => {
+        // entity spawn position
+        entity.pos = new Vec3d(player.x + (1.5 * playerAngle.x), player.y + (player.getEyeHeight() + playerAngle.y), player.z + (1.5 * playerAngle.z))
+        // not affected by gravity
+        entity.setNoGravity(true)
+        const motion = new Vec3d(playerAngle.x * vel, playerAngle.y * vel,  playerAngle.z * vel)
+        // entity.persistentData.put('motion', motion)
+        // entity movement
+        entity.setDeltaMovement(motion)
+    })
+    // do not reduce stack size if in creative
+    if (!player.isCreative()) { item.shrink(1) }
+    // cooldown in seconds
+    let cooldown = 2
+    player.addItemCooldown(item, 20 * cooldown)
+    // player.level.playSound(null, player.x, player.y, player.z, 'minecraft:entity.ghast.shoot', 'players', 1, 1)
+    player.level.playSound(null, player.x, player.y, player.z, 'minecraft:entity.wither.shoot', 'players', 1, 1) // scarier
+    // seconds entity will exist for
+    let entityLife = 30
+    // after number of ticks, entity will be killed
+    Utils.server.scheduleInTicks(20 * entityLife, ctx => {
+            entity.discard()
+    })
+})
 
 
 PlayerEvents.tick(event => {
     const { player, server } = event
     // holding different items will have different effects
     if (player.isHoldingInAnyHand('kubejs:gravitite_gel')) {
-        player.potionEffects.add("minecraft:jump_boost", 1, 2)
+        player.potionEffects.add("minecraft:jump_boost", 1, 1)
     }
     if (player.isHoldingInAnyHand('kubejs:solar_stone')) {
         player.setSecondsOnFire(1)
@@ -40,12 +115,13 @@ PlayerEvents.tick(event => {
         }
     }
     if (player.isHoldingInAnyHand('kubejs:golden_egg')) {
-        player.potionEffects.add("minecraft:luck", 1, 2)
+        player.potionEffects.add("minecraft:luck", 1, 1)
     }
     if (player.isHoldingInAnyHand('kubejs:valkyrean_wing')) {
         player.potionEffects.add("minecraft:strength", 1)
     }
 })
+
 
 BlockEvents.broken(event => {
     const { player, block } = event
@@ -55,7 +131,7 @@ BlockEvents.broken(event => {
         // do not do extra drops to blocks that are not ores
         if (!block.id.includes("_ore")) { return }
         let drops = block.getDrops()
-        let max = 4 // count could be [0-4), so chance of no extra item
+        let max = 1 // count could be [0-1), so chance of no extra item
         for (let i = 0; i < drops.length; i++) {
             let extraItemCount = Math.floor(Math.random() * max)
             // no extra item case
--- kubejs/startup_scripts/progression_items.js
+++ kubejs/startup_scripts/progression_items.js
@@ -1,9 +1,74 @@
+
+StartupEvents.registry('entity_type', event => {
+    event.create('kubejs:solar_stone_projectile', 'entityjs:projectile')
+        /**
+         * One-Off values set at the startup of the game.
+         */
+        .sized(0.4, 0.4)
+        .renderScale(1, 1, 1)
+        .item(item => {
+            item.canThrow(true)
+        })
+        ////Setting .noItem() here will result in the builder skipping the item build altogether
+        ////Since the builder registers the item automatically this is the only way to prevent an item from being created here.
+        // .noItem()
+        ////uncomment if you want it to auto create item (if you havent already)
+        /**
+         * All methods below return void meaning they don't require a set return value to function.
+         * These mostly are similar to KubeJS' normal events where you may do things on certain events your entities call!
+         */
+        .onHitBlock(context => {
+            const { entity, result } = context;
+            // construct explosion
+            let explosion = entity.block.createExplosion()
+            explosion
+                .exploder(entity)
+                .strength(5)
+                .causesFire(true)
+                .explosionMode('tnt') // 'none','block','mob','tnt'
+                .explode()
+            entity.discard()
+        })
+        .onHitEntity(context => {
+            const { entity, result } = context;
+            // custom effect upon hitting entity
+            if (result.entity.living) {
+                result.entity.setSecondsOnFire(10)
+            }
+            let explosion = entity.block.createExplosion()
+            explosion
+                .exploder(entity)
+                .strength(5)
+                .causesFire(true)
+                .explosionMode('tnt')
+                .explode()
+            entity.discard()
+            entity.kill()
+        })
+        .tick(entity => {
+            // if in water, kill entity
+            if (entity.getLevel().getBlockState(entity.blockPosition()).getBlock().id == "minecraft:water") {
+                Utils.server.runCommandSilent(`particle minecraft:angry_villager ${entity.x} ${entity.y} ${entity.z} 0.125 0.125 0.125 1 50 force`)
+                entity.getLevel().playSound(null, entity.x, entity.y, entity.z, 'minecraft:block.fire.extinguish', 'players', 1, 1) // scarier
+                entity.discard()
+            }
+        })
+})
+
 StartupEvents.registry('item', event => {
     // eye of air items
     event.create('gravitite_gel')
         .displayName('Gravitite Gel')
         .maxStackSize(16)
         .tooltip('Feels very light')
+        .food(food => {
+            food
+                .hunger(1)
+                .saturation(1) // real value: min(hunger * saturation * 2 + saturation, foodAmountAfterEating)
+                .effect('minecraft:jump_boost', 20 * 60, 2, 1)
+                .effect('minecraft:speed', 20 * 60, 2, 1)
+                .alwaysEdible()
+        })
     // may need tweaks for balancing
     event.create('valkyrean_wing', 'sword')
         .displayName('Valkyrean Wing')
@@ -17,28 +82,20 @@ StartupEvents.registry('item', event => {
         .tooltip('Hot to the touch')
     // for some code below explained, see: 
     //      https://wiki.latvian.dev/books/kubejs-legacy/page/custom-items#bkmrk-custom-foods
-    // CRASHES THE GAME -- MAYBE ADD AS FOOD AN MAKE IT LIKE A DRINK
-    event.create('whale_wind', 'basic')
-        .tooltip("Strong gust that can blow with force")
-        .useAnimation("drink")
-        .useDuration((itemstack) => 64)
+    // create generic item, then define to be drinkable
+    event.create('whale_wind')
+        .displayName('Whale Wind')
+        .tooltip('Strong gust that can blow with force')
+        .useAnimation('drink')
+        .useDuration(itemstack => 30)
         .use((level, player, hand) => true)
         .finishUsing((itemstack, level, entity) => {
-            let effects = entity.potionEffects;
-            effects.add("minecraft:levitation", 1)
-            itemstack.itemStack.shrink(1)
-            if (entity.player) {
-                entity.minecraftPlayer.addItem(Item.of("minecraft:glass_bottle").itemStack)
-            }
-            return itemstack;
+            let effects = entity.potionEffects
+            effects.add('minecraft:levitation', 20, 100)
+            effects.add('kubejs:falling_immunity', 20 * 20 )
+            itemstack.shrink(1) // reduce item
+            return itemstack
         })
-        .releaseUsing((itemstack) => {
-            itemstack.itemStack.shrink(1)
-        })
-    // event.create('whale_wind')
-    //     .displayName('Whale Wind')
-    //     .maxStackSize(16)
-    //     .tooltip('Strong gust that never stops blowing')
     // eye of bones items
     event.create('green_goo')
         .displayName('Green Goo')
