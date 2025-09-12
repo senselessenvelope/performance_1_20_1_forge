4bde02c6cad00cde32c84ab013386659f690065d
senselessenvelope <seducedenvelope@gmail.com>
2025-09-13 00:00:32 +0100

    changed damageTool to allow for variable damage using recipe details

--- kubejs/startup_scripts/020_recipe_utils.js
+++ kubejs/startup_scripts/020_recipe_utils.js
@@ -4,14 +4,15 @@ let ItemStack = Java.loadClass('net.minecraft.world.item.ItemStack')
 // global utility functions for recipes
 global.recipeUtils = {
     // consume items in recipes or, if they are tools, damage them, if they are containers (like a water bucket) return empty container (bucket)
-    damageTool: function(event, items) {
+    damageTool: function(event, recipe) {
+        let items = recipe.getItemsAsList()
         // for each item that was in recipe
         for (const item of items) {
             // convert to ItemStack Java object (to check if is tool)
             let itemStack = new ItemStack(item)
             if (itemStack.maxDamage > 0) {
                 // damage desired damage specified in recipe (in Recipe class constructor if not specified defaults to global constant)
-                event.damageIngredient(item, 1)
+                event.damageIngredient(item, recipe.getDamage())
             }
         }
     },
@@ -35,7 +36,7 @@ global.recipeUtils = {
             )
         }
         // function to damage tool (containers are updated automatically it seems)
-        global.recipeUtils.damageTool(addedRecipe, recipe.getItemsAsList())
+        global.recipeUtils.damageTool(addedRecipe, recipe)
 
     }
 
