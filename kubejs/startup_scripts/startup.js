325097c7e4c365ec99d9df274b92a4f46db215fd
senselessenvelope <seducedenvelope@gmail.com>
2026-07-06 22:49:48 +0100

    renamed files to be more readable now that priority works

---
+++ kubejs/startup_scripts/startup.js
@@ -0,0 +1,32 @@
+// priority: 100
+
+// ------------------------------
+// -----[ PROGRESSION EYES ]-----
+// ------------------------------
+
+// eyes from legendary monsters mod, this is like an enum for javascript
+global.legendaryMonstersEyes = Object.freeze({
+    EYE_OF_AIR:         "legendary_monsters:eye_of_air", 
+    EYE_OF_MAGMA:       "legendary_monsters:eye_of_magma", 
+    EYE_OF_SOUL:        "legendary_monsters:eye_of_soul",
+    EYE_OF_BONES:       "legendary_monsters:eye_of_bones", 
+    EYE_OF_MANY_RIBS:   "legendary_monsters:eye_of_many_ribs", 
+    EYE_OF_GHOST:       "legendary_monsters:eye_of_ghost",
+    EYE_OF_FROST:       "legendary_monsters:eye_of_frost", 
+    EYE_OF_SANDSTORM:   "legendary_monsters:eye_of_sandstorm", 
+    EYE_OF_MOSS:        "legendary_monsters:eye_of_moss", 
+    EYE_OF_SHULKER:     "legendary_monsters:eye_of_shulker", 
+    EYE_OF_CHORUS:      "legendary_monsters:eye_of_chorus"
+})
+
+// ------------------------------------
+// -----[ PROJECTILE INTERACTION ]-----
+// ------------------------------------
+
+// default time in seconds for effect if not specified for functions
+global.constants = Object.freeze({
+    DEFAULT_TIME:               1,
+    DEFAULT_COOLDOWN:           0,
+    DEFAULT_DAMAGE:             1
+})
+
---
+++ kubejs/startup_scripts/utils/object/object_utils.js
@@ -0,0 +1,15 @@
+// priority: 55
+
+// global utility functions for objects
+global.objectUtils = {
+    // copies all own properties from source to target
+    addObjectData: function(source) {
+        let target = {}
+        for (const key of Object.keys(source)) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                target[key] = source[key]
+            }
+        }
+        return target
+    }
+}
